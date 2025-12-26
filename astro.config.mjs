bdd58d242cda1119c7e031f2866610e510b490d3
Ajith Kumar A R <>
2025-12-26 19:04:45 +0530

    Major Fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 import nodeAdapter from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',             
+  output: 'server',
   adapter: nodeAdapter({ mode: 'standalone' }),
+
+  integrations: [
+    react()
+  ],
+
   vite: {
     plugins: [tailwindcss()]
   }
